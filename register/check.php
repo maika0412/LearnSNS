e3400226dea35f806d49e30322bc699d59115eb3
DESKTOP-MKLTJ8F\maika <maikannu@yahoo.co.jp>
2018-07-19 10:40:32 +0800

    ああああ

--- register/check.php
+++ register/check.php
@@ -1,7 +1,11 @@
 <?php
 session_start();
+if (!isset($_SESSION['register'])) {
+    header('Location:signup.php');
+    exit();
+}
 
-//出力テスト
+//多次元配列　[]がいくつかあるかで決まる。今回は二次元配列。
  $name = $_SESSION['register']['name'];
  $email = $_SESSION['register']['email'];
  $password = $_SESSION['register']['password'];
